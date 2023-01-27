0ccc9b9e278d287a80f1dd972dc903b514dccf22
singhi36 <singhi36@mymacewan.ca>
2023-01-27 15:35:57 -0700

    Add project files.

---
+++ Class2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_3
+{
+    internal class Person : BaseClass
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName = "", string lastName = "")
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override bool IsValidated()
+        {
+            if (FirstName == null)
+            {
+                FirstName = "";
+            }
+            if (LastName == null)
+            {
+                LastName = "";
+            }
+
+            if (FirstName.Length > 0 && LastName.Length > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace assignment_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person bob = new Person();
+            Console.WriteLine(bob.IsValidated());
+
+            Person kim = new Person("kim", "smith");
+            Console.WriteLine(kim.IsValidated());
+        }
+    }
+}
